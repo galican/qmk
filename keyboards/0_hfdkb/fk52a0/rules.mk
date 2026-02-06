3041113a1bf1282c24e8dbdbb8e20978f52d3786
galican <2831039915@qq.com>
2026-02-06 14:38:56 +0800

    Added fk52a2 and fk52a0. 2026/02/06

---
+++ keyboards/0_hfdkb/fk52a0/rules.mk
@@ -0,0 +1,19 @@
+ifeq ($(strip $(CONSOLE_ENABLE)), yes)
+    KEYBOARD_SHARED_EP = no
+endif
+
+RULES_MK_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+
+OPT_DEFS += -DBT_MODE_ENABLE
+OPT_DEFS += -DENTRY_STOP_MODE
+OPT_DEFS += -DNO_USB_STARTUP_CHECK
+
+UART_DRIVER_REQUIRED = yes
+
+SRC += common/bt_task.c
+SRC += common/lp_sleep.c
+SRC += common/retarget_suspend.c
+
+LDFLAGS += -L $(RULES_MK_DIR)/common -l_bts
+
+VPATH += $(RULES_MK_DIR)/common
