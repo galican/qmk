f00efc1896990bd5211da94cdaf84ccc26b56c00
galican <2831039915@qq.com>
2026-04-03 10:56:33 +0800

    Added signalrgb support for hfdkb keyboards. This includes the necessary source files, configuration files, and keymaps for both default and via layouts. The new files are organized under the `keyboards/hfdkb/si2638_signalrgb` directory, with common code for Bluetooth tasks and sleep management in the `common` subdirectory. The commit also includes a JSON file for VIA support and a rules.mk file for build configuration.

---
+++ keyboards/hfdkb/si2638_signalrgb/rules.mk
@@ -0,0 +1,18 @@
+# ifeq ($(strip $(CONSOLE_ENABLE)), yes)
+    KEYBOARD_SHARED_EP = yes
+# endif
+
+OPT_DEFS += -DBT_MODE_ENABLE
+OPT_DEFS += -DWWDG_ENABLE
+OPT_DEFS += -DENTRY_STOP_MODE
+OPT_DEFS += -DNO_USB_STARTUP_CHECK
+
+# QUANTUM_LIB_SRC += uart_serial.c
+UART_DRIVER_REQUIRED = yes
+
+SRC += common/bt_task.c
+SRC += common/lp_sleep.c
+SRC += common/retarget_suspend.c
+
+RULES_MK_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+LDFLAGS += -L $(RULES_MK_DIR)/common -l_bts
