255bf05347a7c08652cf68b24b276d29ee7db4a2
galican <2831039915@qq.com>
2026-09-21 11:10:59 +0800

    Update si2955

--- keyboards/hfdkb/si2955/config.h
+++ keyboards/hfdkb/si2955/config.h
@@ -17,9 +17,9 @@
 #pragma once
 
 #ifdef MULTIMODE_ENABLE
-#    define MM_BT_HOST1_NAME "YUNZII B104PRO -$"
-#    define MM_BT_HOST2_NAME "YUNZII B104PRO -$"
-#    define MM_BT_HOST3_NAME "YUNZII B104PRO -$"
+#    define MM_BT_HOST1_NAME "YUNZII B104 BT$"
+#    define MM_BT_HOST2_NAME "YUNZII B104 BT$"
+#    define MM_BT_HOST3_NAME "YUNZII B104 BT$"
 
 // Indicate index of mm device
 #    define MM_BLINK_HOST1_INDEX 20
